83f4d0e8613efa780b99f8f82aa90bce0984dd66
Qvantt <65224747+Qvantt@users.noreply.github.com>
2024-05-31 17:14:25 +0300

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,26 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalPrice = quantity * pricePerDroid;
-  if (totalPrice > customerCredits) {
-    return `Insufficient funds!`;
-  } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function getCommonElements(array1, array2) {
+  let commonElements = [];
+  for (let i = 0; i < array1.length; i++) {
+    if (array2.includes(array1[i])) {
+      commonElements.push(array1[i]);
+    }
+  }
+
+  return commonElements;
+}
+
+function getCommonElements(array1, array2) {
+  let array3 = [];
+  for (let i = 0; i < array1.length; i++) {
+    if (array2.includes(array1[i])) {
+      array3.push(array1[i]);
+    }
+    return array3;
   }
 }
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+console.log(checkStorage(['apple', 'plum', 'pear'], 'plum')); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
